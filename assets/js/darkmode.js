f7a19ff917d27903244397bcb266098345e2034e
Geoffrey <g.h.garrett13@gmail.com>
2022-05-30 17:05:57 +0200

    Attempting dynamic giscus theme

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,34 +1,34 @@
 const userPref = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
 const currentTheme = localStorage.getItem('theme') ?? userPref
 
-function sendMessage(message) {
-    const iframe = document.querySelector('iframe.giscus-frame');
-    if (!iframe) return;
-    iframe.contentWindow.postMessage({giscus: message}, 'https://giscus.app');
-}
+// function sendMessage(message) {
+//     const iframe = document.querySelector('iframe.giscus-frame');
+//     if (!iframe) return;
+//     iframe.contentWindow.postMessage({giscus: message}, 'https://giscus.app');
+// }
 
 const switchTheme = (e) => {
     if (e.target.checked) {
         document.documentElement.setAttribute('saved-theme', 'dark')
         localStorage.setItem('theme', 'dark')
         // localStorage.setItem('giscus-theme', 'https://giscus.app/themes/dark_dimmed.css')
-        sendMessage({
-            setConfig: {
-                // theme: {{ .Site.Dta.config.comments.giscus.theme.dark }}
-                theme: 'https://giscus.app/themes/dark_dimmed.css'
-            }
-        })
+        // sendMessage({
+        //     setConfig: {
+        //         // theme: {{ .Site.Dta.config.comments.giscus.theme.dark }}
+        //         theme: 'https://giscus.app/themes/dark_dimmed.css'
+        //     }
+        // })
 
     } else {
         document.documentElement.setAttribute('saved-theme', 'light')
         localStorage.setItem('theme', 'light')
         // localStorage.setItem('giscus-theme', 'https://giscus.app/themes/light.css')
-        sendMessage({
-            setConfig: {
-                // theme: {{ .Site.Data.config.comments.giscus.theme.light }}
-                theme: 'https://giscus.app/themes/light.css'
-            }
-        })
+        // sendMessage({
+        //     setConfig: {
+        //         // theme: {{ .Site.Data.config.comments.giscus.theme.light }}
+        //         theme: 'https://giscus.app/themes/light.css'
+        //     }
+        // })
     }
 }
 
