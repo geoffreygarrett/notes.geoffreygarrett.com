66cdff7b633a59ad2e41d7663a6400ba3b3548dc
Geoffrey <g.h.garrett13@gmail.com>
2022-05-31 08:00:49 +0200

    Updated giscus theming

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,59 +1,63 @@
 const userPref = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
 const currentTheme = localStorage.getItem('theme') ?? userPref
-const giscusLoaded = false
 
 function sendMessage(message) {
     const iframe = document.querySelector('iframe.giscus-frame');
-    if (!iframe) {
-        console.log('there is no god')
-    } else {
-        iframe.contentWindow.postMessage({giscus: message}, 'https://giscus.app');
-    }
-}
-
-function giscusTheme(theme) {
-    if (theme === 'dark') {
-        sendMessage({
-            setConfig: {
-                theme: 'https://giscus.app/themes/dark_dimmed.css'
-            }
-        })
-    } else {
-        sendMessage({
-            setConfig: {
-                theme: 'https://giscus.app/themes/light.css'
-            }
-        })
-    }
+    if (!iframe) return;
+    iframe.contentWindow.postMessage({giscus: message}, 'https://giscus.app');
 }
 
 if (currentTheme) {
     document.documentElement.setAttribute('saved-theme', currentTheme);
 }
 
+// handle the switching of giscus theme
+function giscusChangeTheme(theme) {
+    if (theme === 'dark') {
+        const darkTheme = document.getElementsByClassName('giscus-wrapper')[0].getAttribute('data-giscus-dark-theme');
+        sendMessage({
+            setConfig: {
+                theme: (darkTheme ? darkTheme : 'https://giscus.app/themes/dark.css')
+            }
+        })
+    } else {
+        const lightTheme = document.getElementsByClassName('giscus-wrapper')[0].getAttribute('data-giscus-light-theme');
+        sendMessage({
+            setConfig: {
+                theme: (lightTheme ? lightTheme : 'https://giscus.app/themes/light.css')
+            }
+        })
+    }
+}
+
+
 const switchTheme = (e) => {
     if (e.target.checked) {
         document.documentElement.setAttribute('saved-theme', 'dark')
         localStorage.setItem('theme', 'dark')
-        giscusTheme('dark')
+        giscusChangeTheme('dark')
     } else {
         document.documentElement.setAttribute('saved-theme', 'light')
         localStorage.setItem('theme', 'light')
-        giscusTheme('light')
+        giscusChangeTheme('light')
     }
 }
 
+// message handler for giscus -> parent.
 function handleMessage(event) {
     if (event.origin !== 'https://giscus.app') return;
     if (!(typeof event.data === 'object' && event.data.giscus)) return;
     const giscusData = event.data.giscus;
-    giscusTheme(currentTheme)
+    giscusChangeTheme(currentTheme)
+
+    // remove listener after giscus theme changed (only used to set theme).
     window.removeEventListener('message', handleMessage);
-    // You'll need to make sure that `giscusData` contains the message you're
-    // expecting, e.g. by using `if ('discussion' in giscusData)`.
 }
 
+// giscus event listener. This is used to set the theme of the giscus app when
+// giscus is loaded.
 window.addEventListener('message', handleMessage);
+
 
 window.addEventListener('DOMContentLoaded', () => {
     // Darkmode toggle
