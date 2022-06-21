db61311fff33919d8bb4c04d3c0649069dc49ae7
geoffreygarrett <geoffreygarrett@users.noreply.github.com>
2022-06-21 19:40:12 +0000

    deploy: feb78d796063597b7f09e8e6e7a9e0ddfee00cee

---
+++ js/add-on.js
@@ -0,0 +1,16 @@
+
+(function addTitleToCodeBlock() {
+  let list = document.body.getElementsByClassName("highlight");
+
+  for (i = 0; i <= list.length - 1; i++) {
+    let code = list[i].firstElementChild.firstElementChild;
+    let codeName = code ? code.className.split(":")[1] : null;
+
+    if (codeName) {
+      let div = document.createElement("div");
+      div.textContent = codeName;
+      div.classList.add("code-name");
+      code.parentNode.insertBefore(div, code);
+    }
+  }
+}());
