1155dbf0c5df55ad7ef6b38b3ffbb93188c60199
Geoffrey <g.h.garrett13@gmail.com>
2022-06-22 18:23:20 +0200

    Updates

--- assets/js/add-on.js
+++ assets/js/add-on.js
@@ -1,12 +1,16 @@
-var els = document.getElementsByClassName("highlight");
-for (var i = 0; i < els.length; i++) {
-  if (els[i].title.length) {
-    let div = document.createElement("div");
-    div.textContent=els[i].title;
-    div.classList.add("code-name")
-    // var textNode = document.createTextNode(els[i].title);
-    // newNode.appendChild(textNode);
-    // newNode.classList.add("code-name");
-    els[i].parentNode.insertBefore(div, els[i]);
+
+var addon = function(event) {
+  var els = document.getElementsByClassName("highlight");
+  for (var i = 0; i < els.length; i++) {
+    if (els[i].title.length) {
+      let div = document.createElement("div");
+      div.textContent=els[i].title;
+      div.classList.add("code-name")
+      // var textNode = document.createTextNode(els[i].title);
+      // newNode.appendChild(textNode);
+      // newNode.classList.add("code-name");
+      els[i].parentNode.insertBefore(div, els[i]);
+    }
   }
-}
+};
+window.addEventListener("DOMContentLoaded", addon);
